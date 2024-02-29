3f54caee903e24ce67b5144e16932c1fa4bfeefc
Pugialli <joaopugialli@gmail.com>
2024-02-29 14:31:04 -0300

    eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
+import './global.css'
+
+import { Button } from './components/ui/button'
+
 export function App() {
   return (
-    <div>
-      <h1>Pizza Shop</h1>
+    <div className="flex h-screen items-center justify-center">
+      <Button>Enviar</Button>
     </div>
-  );
+  )
 }
